397d9625c8ac38c748d6058b0d569dc7896104f5
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-10-22 10:32:17 +0530

    feat: camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,19 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
 interface CampSiteProps {
   backgroundImage: string;
   title: string;
   subtitle: string;
   peopleJoined: string;
 }
+
+const persons = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
 
 const CampSite = ({
   backgroundImage,
@@ -17,7 +27,29 @@ const CampSite = ({
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
-          <div className="rounded-full bg-green-50 p-4"></div>
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+          <div className="text-white flex gap-1 py-1 flex-col">
+            <h1 className="font-bold text-lg">{title}</h1>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
         </div>
       </div>
     </div>
@@ -31,15 +63,36 @@ const Camp = () => {
         <CampSite
           backgroundImage="bg-bg-img-1"
           title="Lake Mokututso"
-          subtitle="Night Camping"
+          subtitle="Kyoto, Japan"
           peopleJoined="69+ Joined"
         />
         <CampSite
           backgroundImage="bg-bg-img-2"
-          title="Lake Mokututso"
-          subtitle="Night Camping"
+          title="Mountain Range"
+          subtitle="Sweden"
           peopleJoined="69+ Joined"
         />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
       </div>
     </section>
   );
